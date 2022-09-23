9a42b4be1306a5404884e92c3bf8d9f379d5bae9
yessygc <yessgarciacampos@gmail.com>
2022-09-22 22:49:45 -0500

    Promise all

---
+++ 01-fundamentos/prom-3.js
@@ -0,0 +1,40 @@
+
+
+function retornaTrue(){
+    return true;
+}
+
+
+function sumarLento( numero ) {
+
+    return new Promise( function(resolve, reject){
+
+        setTimeout( function() {
+            
+            resolve( numero + 1);
+            //reject( 'Sumar lento falló' );
+
+        },800); 
+    });
+
+}
+
+let sumarRapido = (numero) => {
+
+    return new Promise((resolve, reject) => {
+        setTimeout( ()=> resolve(numero +1 ), 300);
+    });
+}
+
+
+let cosas = [ sumarLento(5), sumarRapido(10), true, 'hola mundo', retornaTrue() ];
+
+Promise.all( cosas )
+        .then( respuestas => {
+            console.log( respuestas );
+        })
+        .catch( console.log );
+
+//sumarLento( 5 ).then( console.log );
+//sumarRapido( 10 ).then( console.log );
+
