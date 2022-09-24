adf46a40c42557dd7b3d96d0df7ddc910406ff52
yessygc <yessgarciacampos@gmail.com>
2022-09-23 21:59:00 -0500

    Response.clone()

---
+++ 01-fundamentos/fetch-5.js
@@ -0,0 +1,25 @@
+
+
+
+fetch('https://reqres.in/api/users/1')
+    .then( resp => {
+        
+        resp.clone().json()
+            .then( usuario => {
+                console.log(usuario.data);
+        });
+
+        resp.clone().json()
+        .then( usuario => {
+            console.log(usuario.data);
+        });
+
+        resp.json().then( usuario => {
+            console.log(usuario.data);
+        });
+
+    });
+    
+    
+
+
